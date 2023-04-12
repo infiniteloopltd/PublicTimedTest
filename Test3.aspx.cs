1eec640bc3327afe110f248eef6f73b9d871736c
Infinite Loop Development Ltd <info@infiniteloop.ie>
2023-04-12 10:33:48 +0200

    ip detection

--- Test3.aspx.cs
+++ Test3.aspx.cs
@@ -1,10 +1,5 @@
 ﻿using MaxMind.GeoIP2;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace ArmiaTest
 {
@@ -14,10 +9,18 @@ namespace ArmiaTest
         {
 
         }
+        private static string GetIpAddress()
+        {
+            var context = System.Web.HttpContext.Current;
+            var ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (string.IsNullOrEmpty(ipAddress)) return context.Request.ServerVariables["REMOTE_ADDR"];
+            var addresses = ipAddress.Split(',');
+            return addresses.Length != 0 ? addresses[0] : context.Request.ServerVariables["REMOTE_ADDR"];
+        }
 
         protected void btnSearch_Click(object sender, EventArgs e)
         {
-            var ip = "2.37.149.2";
+            var ip = GetIpAddress();
             const string strMaxMindKey = "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";
             const int intMaxMindAccountId = 176060;
             var client = new WebServiceClient(intMaxMindAccountId, strMaxMindKey, host: "geolite.info");
